c3a47e4eabe89a6d263c0556613d0bc946b35847
Miguel040404 <mmaldonadocobo4@gmail.com>
2024-12-11 17:50:03 +0100

    funciona casas de medicos

--- src/app/medicos-bd/page.jsx
+++ src/app/medicos-bd/page.jsx
@@ -1,10 +1,25 @@
 import Link from "next/link";
 import connection from "../lib/mysql";
+import { revalidatePath } from "next/cache";
 
 
+async function eliminarMedicos(formData) {
+    'use server'
+const id = formData.get('id')
 
+await connection.query('DELETE FROM medicos WHERE id = ?', [id])
+}
 
+async function insertarAlumno(formData) {
+    'use server'
+    const nombre = formData.get('nombre')
+    const especialidad = formData.get('especialidad')
+    const perfil = formData.get('perfil')
 
+    await connection.query('INSERT INTO medicos (nombre, especialidad, perfil) VALUES (?, ?, ?)',
+        [nombre, especialidad, perfil])
+        revalidatePath('/medicos-bd')
+}
 
 async function PaginaMedicos(params) {
     
@@ -12,8 +27,16 @@ async function PaginaMedicos(params) {
 
     return (
 
-      <div>  Medicos
+      <>  Medicos
 
+        <form action={insertarAlumno}>
+            <input type="text" name="nombre" />
+            <input type="text" name="especialidad" />
+            <input type="text" name="perfil" />
+            <button>Insertar</button>
+        </form>
+
+<div>
         {
 
             rows.map(medicos=>
@@ -27,8 +50,8 @@ async function PaginaMedicos(params) {
                 </div>
             )
         }
-
-      </div>
+</div>
+      </>
     );
 
 }
