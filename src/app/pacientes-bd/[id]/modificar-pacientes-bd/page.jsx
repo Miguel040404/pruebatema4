d6767e439e91da2c1a47bb79eec4de9c1476da2c
Miguel040404 <mmaldonadocobo4@gmail.com>
2024-12-11 18:34:59 +0100

    pacientes funciona

--- src/app/pacientes-bd/[id]/modificar-pacientes-bd/page.jsx
+++ src/app/pacientes-bd/[id]/modificar-pacientes-bd/page.jsx
@@ -1,32 +1,32 @@
 import connection from "@/lib/mysql";
 import { redirect } from "next/navigation";
 
-async function modificarMedicos(formData) {
+async function modificarPacientes(formData) {
     'use server'
     const id = formData.get('id');
     const nombre = formData.get('nombre');
-    const especialidad = formData.get('especialidad');
-    const perfil = formData.get('perfil');
+    const localidad = formData.get('localidad');
+    const fecha_nacimiento = formData.get('fecha_nacimiento');
 
-    await connection.query('UPDATE medicos SET nombre = ?, especialidad = ?, perfil = ? WHERE id = ?',
-        [nombre, especialidad, perfil, id])
+    await connection.query('UPDATE pacientes SET nombre = ?, localidad = ?, fecha_nacimiento = ? WHERE id = ?',
+        [nombre, localidad, fecha_nacimiento, id])
 
-    redirect(`/medicos-bd/${id}`);
+    redirect(`/pacientes-bd/${id}`);
 }
 
 async function PaginaModificar({ params }) {
 
     const { id } = await params;
-    const [rows] = await connection.query('SELECT * FROM medicos WHERE id = ?', [id]);
-    const medicos = rows[0];
+    const [rows] = await connection.query('SELECT * FROM pacientes WHERE id = ?', [id]);
+    const pacientes = rows[0];
 
     return (
         <>
-            <form action={modificarMedicos}>
-                <input type="hidden" name="id" value={medicos.id} />
-                <input type="text" name="nombre" defaultValue={medicos.nombre} />
-                <input type="text" name="especialidad" defaultValue={medicos.especialidad} />
-                <input type="text" name="perfil" defaultValue={medicos.perfil} />
+            <form action={modificarPacientes}>
+                <input type="hidden" name="id" value={pacientes.id} />
+                <input type="text" name="nombre" defaultValue={pacientes.nombre} />
+                <input type="text" name="localidad" defaultValue={pacientes.localidad} />
+                <input type="date" name="fecha_nacimiento" defaultValue={pacientes.fecha_nacimiento.toISOString().split('T')[0]} />
                 <button >Modificar</button>
 
             </form>
